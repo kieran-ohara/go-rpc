4db8399d030e660d1051c8207b64ee137b791910
Kieran Bamforth <hello@kieranbamforth.me>
2022-11-16 19:11:50 +0000

    what comes from where.

--- hello.go
+++ hello.go
@@ -6,14 +6,14 @@ import "github.com/kieran-ohara/gogreet"
 import "github.com/kieran-ohara/gogreet/greetrpcdist"
 
 func main() {
-	message, err := gogreet.Hello("")
+	message, err := gogreet.Hello("this is a Hello name")
 	if err != nil {
 		fmt.Println("there was an error")
 	}
 	fmt.Println(message)
 
   greetDevice := greetrpcdist.Greet {
-    Name: "this is a name",
+    Name: "this is Greet name",
   }
 	fmt.Println(greetDevice.Name)
 }
