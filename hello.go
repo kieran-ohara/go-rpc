805cc84ddefc3bcfdf37bd3a2a2736f6bf49f517
Kieran Bamforth <hello@kieranbamforth.me>
2022-11-16 21:00:31 +0000

    Create a server.

--- hello.go
+++ hello.go
@@ -1,9 +1,10 @@
 package main
 
-import "fmt"
-
 import "github.com/kieran-ohara/gogreet"
 import "github.com/kieran-ohara/gogreet/greetrpcdist"
+import "github.com/kieran-ohara/gogreet/server"
+
+import "fmt"
 
 func main() {
 	message, err := gogreet.Hello("this is a Hello name")
@@ -16,4 +17,6 @@ func main() {
     Name: "this is Greet name",
   }
 	fmt.Println(greetDevice.Name)
+
+  server.Serve()
 }
